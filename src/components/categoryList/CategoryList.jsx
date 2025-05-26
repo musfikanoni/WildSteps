6a8c3d3c0206b21d285cb3c1384ec348fdf397a7
musfikanoni <musfikanoni@gmail.com>
2025-05-27 01:37:31 +0600

    created cateries sections

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import Link from "next/link";
+import styles from "./categoryList.module.css";
+import Image from "next/image";
 
 const CategoryList = () => {
     return (
-        <div>
-            categoryList
+        <div className={styles.container}>
+            <h1 className={styles.title}>Popular Categories</h1>
+            <div className={styles.categories}>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png"
+                        className={styles.image}
+                        width={32} height={32} alt="" />
+                    style
+                </Link>
+
+                <Link href={`blog`}  className={`${styles.category} ${styles.fashion}`}>
+                    <Image src="/fashion.png"
+                        className={styles.image}
+                        width={32} height={32} alt="" />
+                        Fashion
+                </Link>
+
+                <Link href={`blog`}  className={`${styles.category} ${styles.food}`}>
+                    <Image src="/food.png"
+                        className={styles.image}
+                        width={32} height={32} alt="" />
+                    Food
+                </Link>
+
+                <Link href={`blog`} className={`${styles.category} ${styles.travel}`}>
+                    <Image src="/travel.png"
+                        className={styles.image}
+                        width={32} height={32} alt="" />
+                    style
+                </Link>
+
+                <Link href={`blog`} className={`${styles.category} ${styles.culture}`}>
+                    <Image src="/culture.png"
+                        className={styles.image}
+                        width={32} height={32} alt="" />
+                    Culture
+                </Link>
+
+                <Link href={`blog`} className={`${styles.category} ${styles.coding}`}>
+                    <Image src="/coding.png"
+                        className={styles.image}
+                        width={32} height={32} alt="" />
+                    Stories
+                </Link>
+
+            </div>
         </div>
     );
 };
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './featured.module.css';
 import Image from 'next/image';
 
